68830e8dc805889688611d3f3b964866b1c3905f
Aman Shaikh <70093492+aman983@users.noreply.github.com>
2023-04-16 19:05:32 +0530

    updated to read digital input and control relay

--- plc.py
+++ plc.py
@@ -1,59 +1,144 @@
 import socket
+class PLC():    
+    def __init__(self,IP,PORT):
+        self.IP = IP
+        self.PORT = PORT
+        self.Command = []
+        self.type = ""
+    def Connect(self):
+        self.dev = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.dev.connect((self.IP,self.PORT))
+        #If not connected thed add error 
+    def Send_Command(self,cmd):
+        self.dev.send(bytes(cmd))
+    def Recive_Data(self):
+        data = self.dev.recv(1024)
+        self.input_buffer = repr(data)
+        if (self.input_buffer[2:-3] == ">"):
+            print("From PLC --> ",self.input_buffer[2:-3])
 
-plc = "10.0.1.88"
-port = 10002
+        elif (self.type == "Digital I/O"):
+            print("From PLC --> ",self.input_buffer[3:-3])
+            return {"Relay Status":self.input_buffer[3:5],"Digital Input status":self.input_buffer[5:-3]}
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((plc,port))
+        elif (self.type == "Counter Value"):
+            print("From PLC --> ",self.input_buffer[5:-3])
+            return {"Device":self.input_buffer[3:5],"Digital Input status":self.input_buffer[5:-3]}
+        
+        elif (self.type == "Counter Value"):
+            print("From PLC --> ",self.input_buffer[5:-3])
+            return {"Device":self.input_buffer[3:5],"Digital Input status":self.input_buffer[5:-3]}
+        
 
-relay = []
-def relay_control(value):
-    global relay
-    rly = value
-    relay = [0x40,0x30,0x30,0x31,0x0D]
+        self.dev.close()
+    def Relay_control(self,rly):
+        self.Connect()
+        self.command = [0x40,0x30,0x30,0x31,0x0D]
 
-    if(rly==0):
-        relay[3] = 0x30
-    elif (rly==1):
-        relay[3] = 0x31
-    elif (rly==2):
-        relay[3] = 0x32
-    elif (rly==3):
-        relay[3] = 0x34
-    elif (rly==4):
-        relay[3] = 0x38
+        if(rly==0):
+            self.command[3] = 0x30
+        elif (rly==1):
+            self.command[3] = 0x31
+        elif (rly==2):
+            self.command[3] = 0x32
+        elif (rly==3):
+            self.command[3] = 0x34
+        elif (rly==4):
+            self.command[3] = 0x38
 
-    elif (rly==12 or rly==21):
-        relay[3] = 0x33
-    elif (rly==13 or rly==31):
-        relay[3] = 0x35
-    elif (rly==14 or rly==41):
-        relay[3] = 0x39
-    elif (rly==23 or rly==32):
-        relay[3] = 0x36
-    elif (rly==24 or rly==42):
-        relay[3] = 0x36
-    elif (rly==34 or rly==43):
-        relay[3] = 0x43
+        elif (rly==12 or rly==21):
+            self.command[3] = 0x33
+        elif (rly==13 or rly==31):
+            self.command[3] = 0x35
+        elif (rly==14 or rly==41):
+            self.command[3] = 0x39
+        elif (rly==23 or rly==32):
+            self.command[3] = 0x36
+        elif (rly==24 or rly==42):
+            self.command[3] = 0x36
+        elif (rly==34 or rly==43):
+            self.command[3] = 0x43
 
-    elif (rly==123 or rly==132 or rly==213 or rly==231 or rly==321 or rly==312):
-        relay[3] = 0x37
+        elif (rly==123 or rly==132 or rly==213 or rly==231 or rly==321 or rly==312):
+            self.command[3] = 0x37
 
-    elif (rly==234 or rly==243 or rly==324 or rly==342 or rly==432 or rly==423):
-        relay[3] = 0x45
+        elif (rly==234 or rly==243 or rly==324 or rly==342 or rly==432 or rly==423):
+            self.command[3] = 0x45
 
-    elif (rly==134 or rly==143 or rly==413 or rly==431 or rly==341 or rly==314):
-        relay[3] = 0x44
+        elif (rly==134 or rly==143 or rly==413 or rly==431 or rly==341 or rly==314):
+            self.command[3] = 0x44
 
-    elif (rly==124 or rly==142 or rly==412 or rly==421 or rly==241 or rly==214):
-        relay[3] = 0x42
+        elif (rly==124 or rly==142 or rly==412 or rly==421 or rly==241 or rly==214):
+            self.command[3] = 0x42
 
-    elif (rly==1234):
-        relay[3] = 0x46
-    return relay
+        elif (rly==1234):
+            self.command[3] = 0x46
+        self.Send_Command(self.command)
+        self.type = "Digital I/O"
+        self.Recive_Data()
+    def Digital_Input(self):
+        self.Connect()
+        self.command = [0x40,0x30,0x30,0x0D]
+        self.Send_Command(self.command)
+        self.ret = self.Recive_Data()
+        return self.ret
+    def Counter_Value(self,pin):
+        self.type = "Counter Value"
+        self.Connect()
+        if (pin == 1):
+            self.command = [0x20,0x23,0x30,0x30,0x30,0x0D]
+        elif (pin == 2):
+            self.command = [0x20,0x23,0x30,0x30,0x31,0x0D]
+        elif (pin == 3):
+            self.command = [0x20,0x23,0x30,0x30,0x32,0x0D]
+        elif (pin == 4):
+            self.command = [0x20,0x23,0x30,0x30,0x33,0x0D]
+        self.Send_Command(self.command)
+        return self.Recive_Data()
+    def Reset_Counter(self,counter):
+        self.Connect()
+        if (counter == 1):
+            self.command = [0x20,0x24,0x30,0x30,0x43,0x30,0x0D]
+        elif (counter == 2):
+            self.command = [0x20,0x24,0x30,0x30,0x43,0x31,0x0D]
+        elif (counter == 3):
+            self.command = [0x20,0x24,0x30,0x30,0x43,0x32,0x0D]
+        elif (counter == 4):
+            self.command = [0x20,0x24,0x30,0x30,0x43,0x33,0X0D]
+        self.Send_Command(self.command)
+        return self.Recive_Data()
+    def Status_Latch(self,pin):
+        self.Connect()
+        if (pin == 1):
+            self.command = [0x20,0x24,0x30,0x30,0x4c,0x30,0X0D]
+        elif (pin == 2):
+            self.command = [0x20,0x24,0x30,0x30,0x4c,0x31,0X0D]
+        elif (pin == 3):
+            self.command = [0x20,0x24,0x30,0x30,0x4c,0x32,0X0D]
+        elif (pin == 4):
+            self.command = [0x20,0x24,0x30,0x30,0x4c,0x33,0X0D]
+        self.Send_Command(self.command)
+        return self.Recive_Data()
+    def Clear_Latch_Status(self):
+        self.Connect()
+        self.command = [0X20,0x24,0x30,0x30,0x43,0X0D]
+        self.Send_Command(self.command)
+        self.ret = self.Recive_Data()
+        print(self.ret)
+        return self.ret
 
-relay_control(4)
-s.send(bytes(relay))
-data = s.recv(1024)
-s.close()
-print(repr(data))
+
+#plc_name = PLC(IP Address , Port number)
+demo_room1 = PLC("10.0.1.88", 10002)
+
+demo_room1.Relay_control(1234) # To control relay ports of the PLC 
+
+demo_room1.Digital_Input() # Get digital input of PLC
+
+demo_room1.Counter_Value(1) #Get counter value of digital input 1 
+
+demo_room1.Reset_Counter(1) # Reset the counter 1 value
+
+demo_room1.Status_Latch(1) # Get latch status of pin 1
+
+demo_room1.Clear_Latch_Status() #C lears latch status 
